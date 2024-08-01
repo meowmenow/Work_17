3a3d5db0a2c331684e329c67b2754243ddd9b8b0
dartg <dartg@DESKTOP-32CQM3V>
2024-08-01 18:24:13 +0300

    Добавьте файлы проекта.

---
+++ Адские исключения part.2/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите путь файла:");
+            string filePath = Console.ReadLine(); // Записывает путь файла в строку
+
+            using (StreamReader sr = new StreamReader(filePath)) // Читает файл 
+            {
+                string fileContent = sr.ReadToEnd(); // Читаем
+
+                Console.WriteLine(fileContent); // Вывод из файла
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine("Кажется вы не самурай и пути у вас нет =(");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Куда я жмал?");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
